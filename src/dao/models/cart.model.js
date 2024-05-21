f6af78fb6d353e0d5f1235bb4e3db41cfcaac9de
Ripcv <gefallene.engel@gmail.com>
2024-05-21 19:12:34 -0400

    Se elimina require del cartSchema

--- src/dao/models/cart.model.js
+++ src/dao/models/cart.model.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const cartCollection = "Carritos"
 
 const cartSchema = new mongoose.Schema({
-    user: {type: String, required:true, max:100},
+    user: {type: String,  max:100},
     products: [ 
         {
-            product: {type: String, required:true},
+            product: {type: String},
             quantity: { type: Number , default:1}
         }
     ]
